bf92c12ab1b775852ed667e0d5e512639eea2d8a
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2022-05-11 10:15:06 -0700

    test

---
+++ app.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('.section')
+const sectBtns = document.querySelectorAll('.controlls')
+const sectBtn = document.querySelectorAll('.control')
+const allSelections = document.querySelectorAll('.main-content')
+
+
+function PageTransition (){
+    //button click active class
+    for(let i =0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', ()=>{
+            let currentBtn = document.
+            
+        })
+    }
+
+}
